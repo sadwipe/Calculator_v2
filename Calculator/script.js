a0c74560ba715e362aff40fce4733da070858f10
sadwipe <sadwipe@gmail.com>
2023-10-22 14:17:48 +0300

    first version

--- Calculator/script.js
+++ Calculator/script.js
@@ -5,26 +5,85 @@ const allClear = document.querySelector(".all-clear");
 const changeSign = document.querySelector(".change-sign");
 const percent = document.querySelector(".percent");
 const comma = document.querySelector(".comma");
+const equal = document.querySelector(".equal");
+const currentTime = document.querySelector(".current-time");
 
 // result panel
 const currentNumberLabel = document.querySelector(".current-number");
+
 let hasComma = false;
+let hasOperator = false;
+
+let sign;
+let currentNumber;
+
+function updateClock() {
+  const now = new Date();
+
+  const hours = String(now.getHours()).padStart(2, "0");
+  const minutes = String(now.getMinutes()).padStart(2, "0");
+
+  const currentTimeString = `${hours}:${minutes}`;
+
+  currentTime.textContent = currentTimeString;
+}
+
+function getNumber() {
+  currentNumber = +currentNumberLabel.textContent;
+  hasOperator = false;
+}
 
 function updateLabel(value) {
-  let size = currentNumberLabel.textContent.length;
-  if(size >= 7) {
-    currentNumberLabel.style.cssText = "font-size: 4rem";
+  if (currentNumberLabel.textContent[0] == 0 && value == ".") {
+    currentNumberLabel.textContent += value;
+    return;
   }
-  if (size >= 10) {
-    currentNumberLabel.style.cssText = "font-size: 3.5rem";
-  }
-  if (currentNumberLabel.textContent[0] == 0) {
+  if (
+    currentNumberLabel.textContent[0] == 0 &&
+    currentNumberLabel.textContent[1] != "."
+  ) {
     currentNumberLabel.textContent = "";
   }
-  if(currentNumberLabel.textContent.length < 11) {
+  if (hasOperator) {
+    getNumber();
+    hasComma = false;
+    currentNumberLabel.textContent = "";
+    currentNumberLabel.textContent += value;
+    return;
+  }
+  if (currentNumberLabel.textContent.length < 9) {
+    currentNumberLabel.textContent += value;
+    return;
+  }
+  if (currentNumberLabel.textContent.length < 10 && hasComma) {
     currentNumberLabel.textContent += value;
   }
 }
+
+function calculate(a, b, sign) {
+  switch (sign) {
+    case "/":
+      return a / b;
+    case "*":
+      return a * b;
+    case "-":
+      return a - b;
+    case "+":
+      return a + b;
+  }
+}
+
+changeSign.addEventListener("click", () => {
+  currentNumber = -+currentNumberLabel.textContent;
+  currentNumberLabel.textContent = currentNumber;
+});
+
+operators.forEach((operator) => {
+  operator.addEventListener("click", (e) => {
+    sign = e.target.getAttribute("value");
+    hasOperator = true;
+  });
+});
 
 digits.forEach((digit) => {
   digit.addEventListener("click", () => {
@@ -39,3 +98,17 @@ comma.addEventListener("click", () => {
     hasComma = true;
   }
 });
+
+equal.addEventListener("click", () => {
+  if (sign != undefined) {
+    let secondNumber = +currentNumberLabel.textContent;
+    currentNumberLabel.textContent = calculate(
+      currentNumber,
+      secondNumber,
+      sign
+    );
+  }
+});
+
+updateClock();
+setInterval(updateClock, 1000);
